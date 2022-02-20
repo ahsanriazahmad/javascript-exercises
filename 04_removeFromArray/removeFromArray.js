705b83a24d757fdaaba7ab0d479c587ca02805f4
ahsanriazahmad <ahsanriazahmad1@gmail.com>
2022-02-19 22:31:35 -0800

    understood for to remove a single element from array and the ...arg case. completed sumAll as well.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,28 @@
-const removeFromArray = function() {
+//Worked for a single argument but not multiple
+// const removeFromArray = function (list, removeNum) {
+//   const newArray = [...list];
+//   for (let i = 0; i < list.length; i++) {
+//     if (newArray[i] == removeNum) {
+//       newArray.splice(i, 1);
+//       return newArray;
+//     }
+//   }
+// };
 
+var removeFromArray = function (...args) {
+  const array = args[0];
+  return array.filter((removeVal) => !args.includes(removeVal));
 };
+
+// const removeFromArray = function (list, removeNum) {
+//   const newArray = [...list];
+//   for (let i = 0; i < list.length; i++) {
+//     if (newArray[i] == removeNum) {
+//       newArray.filter(i);
+//       return newArray;
+//     }
+//   }
+// };
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  let result = 0;
+  if ((start || end) === null) {
+    return "ERROR";
+  } else if ((start || end) < 0) {
+    return "ERROR";
+  } else if (typeof start == "string" || typeof end == "string") {
+    return "ERROR";
+  } else if (Array.isArray(start) || Array.isArray(end)) {
+    return "ERROR";
+    //   } else if (typeof (start || end) == "string") {
+    //     return "ERROR";
+  } else if (start > end) {
+    for (let i = end; i <= start; i++) {
+      result += i;
+    }
+  } else
+    for (let i = start; i <= end; i++) {
+      result += i;
+    }
+  return result;
 };
 
 // Do not edit below this line
