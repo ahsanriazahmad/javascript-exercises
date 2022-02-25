3047b236bf63d8c227e585bf7d3b118021bd2774
ahsanriazahmad <ahsanriazahmad1@gmail.com>
2022-02-24 21:22:58 -0800

    completed 06 and 07, all test passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function (FD) {
+  let result = round((FD - 32) * (5 / 9), 1);
+  //if (result % )
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function (CL) {
+  let result = round(CL * (9 / 5) + 32, 1);
+  return result;
 };
+
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
